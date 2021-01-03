51c12e63ffe26b45b3cd50b08de51f44c0ba107a
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-01-03 18:13:24 +0000

    the any type

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -36,3 +36,8 @@ const logNumber: (i: number) => void = (i: number) => {
 }
 
 
+//when to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y":20}';
+const coordinates = JSON.parse(json);
+console.log(coordinates); 
