006d61f0431e7267d782d12f4274ec49bfeb7fe4
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-01-03 18:21:14 +0000

    when inference doesn't work

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -52,3 +52,14 @@ for(let i =0; i < words.length; i++) {
     }
 }
 
+
+// variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for(let i =0; i < numbers.length; i++) {
+    if(numbers[i] >0) {
+        numberAboveZero = numbers[i];
+    }
+}
+
