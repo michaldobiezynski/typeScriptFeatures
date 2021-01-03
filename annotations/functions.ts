296850817c2fe88b50567a2ba631a4bc13455e8d
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-01-03 18:42:36 +0000

    annotations for anonymouse functions

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -6,3 +6,10 @@ const substract = (a: number, b: number): number => {
   return a - b;
 };
 
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
