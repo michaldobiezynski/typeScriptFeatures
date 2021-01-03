9ff011a610064b29656fefe656467f2a67f687d3
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-01-03 19:13:20 +0000

    multiple typees in arrays

--- arrays.ts
+++ arrays.ts
@@ -12,5 +12,10 @@ carMakers.push(100);
 
 //Help with 'map' etc
 carMakers.map((car: string): string => {
-    return car
-})
+  return car;
+});
+
+// flexible types
+const importantDates: (Date | string)[] = [new Date(), "2030-10-10"];
+importantDates.push("2030-11-10");
+importantDates.push(new Date());
