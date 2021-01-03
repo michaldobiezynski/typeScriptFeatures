2c4197d84a553a8147b1fdcb2fc68962be6ad96c
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-01-03 18:36:16 +0000

    inference around functions

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -1,3 +1,8 @@
-const add = (a: number, b: number): number => {
+const add = (a: number, b: number) => {
   return a + b;
 };
+
+const substract = (a: number, b: number): number => {
+  return a - b;
+};
+
