0b077685173a3d7117e7d710381f0b775b907a36
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-01-04 10:54:28 +0000

    fields in classes

--- classes.ts
+++ classes.ts
@@ -1,28 +1,31 @@
 class Vehicle {
-//   public drive(): void {
-//     console.log("chugga chugga");
-//   }
+  constructor(public color: string) {}
+
+  //   public drive(): void {
+  //     console.log("chugga chugga");
+  //   }
   protected honk(): void {
     console.log("beep");
   }
 }
 
-class Car extends Vehicle {
-  private drive(): void {
-    console.log("Vroom");
-  }
+// class Car extends Vehicle {
+//   private drive(): void {
+//     console.log("Vroom");
+//   }
 
-  startDrivingProcess(): void {
-    this.drive;
-    this.honk()
-  }
-}
+//   startDrivingProcess(): void {
+//     this.drive;
+//     this.honk();
+//   }
+// }
 
-const vehicle = new Vehicle();
+const vehicle = new Vehicle("orange");
 // vehicle.drive();
 // vehicle.honk();
+console.log(vehicle.color);
 
-const car2 = new Car();
+// const car2 = new Car();
 // car2.drive();
-car2.startDrivingProcess();
+// car2.startDrivingProcess();
 // car2.honk();
