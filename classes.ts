a480527b7c7049aca9afd00ade6dbe325535c5b9
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-01-04 10:49:23 +0000

    class method modifiers

--- classes.ts
+++ classes.ts
@@ -1,22 +1,28 @@
 class Vehicle {
-  drive(): void {
-    console.log("chugga chugga");
-  }
-  honk(): void {
+//   public drive(): void {
+//     console.log("chugga chugga");
+//   }
+  protected honk(): void {
     console.log("beep");
   }
 }
 
 class Car extends Vehicle {
-  drive(): void {
+  private drive(): void {
     console.log("Vroom");
+  }
+
+  startDrivingProcess(): void {
+    this.drive;
+    this.honk()
   }
 }
 
 const vehicle = new Vehicle();
-vehicle.drive();
-vehicle.honk();
+// vehicle.drive();
+// vehicle.honk();
 
 const car2 = new Car();
-car2.drive();
-car2.honk();
+// car2.drive();
+car2.startDrivingProcess();
+// car2.honk();
